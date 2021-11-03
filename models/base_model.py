1e546d43c7e241ffb48f90e26e4f8e7e864a4f21
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-03 17:12:29 -0500

    [UPDATE]

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
 """ class Base model for AirBnB clone """
+import models
+from uuid import uuid4
+from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
     """ Base model 
         id:
         created_at:
         updtaed_at:
     """
 
-    save(self):
+    def __init__(self, *args, *kwargs):
+        """ Object atributes """
 
-    to_dict(self):
+        if len(kwargs) == 0:
+            self.id = str(uuid4())
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                   "%Y-%m-%dT%H:%M:%S.%f")
 
+    def __str__(self):
+        """ Instance print """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                self.__dict__)
 
+    def save(self):
+        """ Save changes """
+        self.updated_at = datetime.now()
+       
 
-    __str__:
-
-	[<class name>] (<self.id>) <self.__dict__>
-
-
+    def to_dict(self):
+        """ Dictionary of model """
+        dictionary = dict(self.__dict__)
+        return dictionary
 
