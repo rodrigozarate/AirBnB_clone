74af4ee07794923406014fb70ee7933e509ad2c5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-03 21:49:00 -0500

    [UPDATE]

--- models/base_model.py
+++ models/base_model.py
@@ -26,6 +26,10 @@ class BaseModel:
             kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
                                    "%Y-%m-%dT%H:%M:%S.%f")
 
+            for key, value in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, value)
+
     def __str__(self):
         """ Instance print """
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
@@ -39,5 +43,8 @@ class BaseModel:
     def to_dict(self):
         """ Dictionary of model """
         dictionary = dict(self.__dict__)
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dictionary['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return dictionary
 
