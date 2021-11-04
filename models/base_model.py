bb5792479048922beb76122418605c487d6670d6
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-03 21:07:02 -0500

    [UPDATE]

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,18 @@ class BaseModel:
         updtaed_at:
     """
 
-    def __init__(self, *args, *kwargs):
+    def __init__(self, *args, **kwargs):
         """ Object atributes """
 
         if len(kwargs) == 0:
             self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                   "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
                                    "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
@@ -26,10 +31,10 @@ class BaseModel:
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                 self.__dict__)
 
+
     def save(self):
         """ Save changes """
         self.updated_at = datetime.now()
-       
 
     def to_dict(self):
         """ Dictionary of model """
