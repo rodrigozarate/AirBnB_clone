2239d20fecb9acfee246ca15bdec11c3e2ed842e
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-03 21:44:50 -0500

    [CREATE]

---
+++ models/engine/file_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Model storage serialize and deserialization """
+
+
+class FileStorage:
+    """ save the data to file """
+    def all(self):
+        """ return dictionary objects """
+
+    def new(self, obj):
+        """ set objects with keys """
+
+    def save(self):
+        """ serializes to JSON """
+
+    def reload(self):
+        """ deserialize JSON to object """
