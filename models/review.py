600c86841832c5be114d2bfa521fd4e5b3684149
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-05 14:09:56 -0500

    [CREATE]

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" MODULE review for AirBnB clone console """
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """ Review class """
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
