d021d6ac27362bb97c465c6a7c7a3ea26369430e
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-05 13:42:00 -0500

    [UPDATE]

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,14 +4,24 @@
 
 class FileStorage:
     """ save the data to file """
+
+    __file_path = "data.json"
+    __objects = {}
+
     def all(self):
         """ return dictionary objects """
+        return self.__objects
 
     def new(self, obj):
         """ set objects with keys """
 
     def save(self):
         """ serializes to JSON """
+        json_obj = {}
+        for key in self.__objects:
+            json_obj[key] = self._objects[key].to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(json_obj, file, indent=2)
 
     def reload(self):
         """ deserialize JSON to object """
