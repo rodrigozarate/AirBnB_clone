2f519a9d415c38a7e0afa892f617de3fd6daa6bf
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-07 17:59:45 -0500

    [UPDATE]

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,18 +4,19 @@ import unittest
 import sys
 import re
 import random
+from io import StringIO
 from console import HBNBCommand
 from models import *
 from unittest.mock import patch
 
 
-class TestConsole(unittest.Testcase):
+class TestConsole(unittest.TestCase):
     """ Main class to test console """
     def test_help(self):
         expected = """
 Documented commands (type help <topic>):
 ========================================
-EOF  all  create  destroy  help  quit  show  update
+help
 \n"""
         with patch('sys.stdout', new=StringIO()) as str:
             HBNBCommand().onecmd("help")
