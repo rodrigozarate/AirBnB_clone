886ddc62043f8ab133585aa2d4c2ae9305429ef9
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-08 09:56:11 -0500

    [FIX]

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.user import User
+
+
+class TestUser_instantiation(unittest.TestCase):
+    """Unittests instantiation User """
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(User, type(User()))
