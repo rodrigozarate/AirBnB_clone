c0430c3e897b02525f670e6269649e41c94babef
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-08 09:54:12 -0500

    [FIX]

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """ Unittests instantiation BaseModel """
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
