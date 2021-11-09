2e5e418940ab1b6c257c56ab30c58697dad63d2f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-11-08 22:32:06 -0500

    [FIX]

--- models/user.py
+++ models/user.py
@@ -5,10 +5,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ User class """
-    first_name = ""
-    last_name = ""
     email = ""
     password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
